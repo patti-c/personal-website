0ed8db9f9c08dc4327a3fbb563c766ed25273838
patti-c <patricia4194@gmail.com>
2018-12-19 15:20:58 -0500

    starfield basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Starfield from './containers/Starfield/Starfield.js'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        
+        <Starfield />
       </div>
     );
   }
